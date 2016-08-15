e3c63b0b05dc39494a1abed2d0d80f77854eca64
Michael Schmidt <github@mschmidt.me>
2016-08-15 15:05:32 +0200

    Delay theme switching slightly to allow UISwitch to finish its state animation

--- Refracto/SettingsController.m
+++ Refracto/SettingsController.m
@@ -210,7 +210,10 @@ static NSString *kStoreURL           = @"https://itunes.apple.com/app/id95498182
 
     if ([sender respondsToSelector:@selector(isOn)]) {
 
-        [AppDelegate appDelegate].darkInterface = [sender isOn];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+
+            [AppDelegate appDelegate].darkInterface = [sender isOn];
+        });
     }
 }
 
